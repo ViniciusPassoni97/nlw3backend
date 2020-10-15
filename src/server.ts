a764fea153545fe5ef20dbb5be163afbb25aab56
ViniciusPassoni97 <43391455+ViniciusPassoni97@users.noreply.github.com>
2020-10-14 22:26:41 -0300

    Acessando urls de imagens

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import routes from './routes/index';
 import './database/connection';
+import path from 'path';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname,'..','uploads')))
 
 app.listen(3333);
