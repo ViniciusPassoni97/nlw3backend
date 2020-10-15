f27ef329e4bd88b761d20aedb7a18a600d2cc380
ViniciusPassoni97 <43391455+ViniciusPassoni97@users.noreply.github.com>
2020-10-14 22:38:24 -0300

    Lidando com exceções

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import routes from './routes/index';
 import './database/connection';
 import path from 'path';
+import errorHandle from './errors/handle';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname,'..','uploads')))
+app.use(errorHandle);
 
 app.listen(3333);
