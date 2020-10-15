4628df3c33b628cb956d6390a6422a16d65b42ba
ViniciusPassoni97 <43391455+ViniciusPassoni97@users.noreply.github.com>
2020-10-14 23:12:53 -0300

    Fazendo mais tratativas de erros

--- src/errors/handle.ts
+++ src/errors/handle.ts
@@ -1,6 +1,20 @@
 import {ErrorRequestHandler} from 'express';
+import {ValidationError} from 'yup';
+
+interface ValidationErros {
+    [key:string]:string[];
+}
 
 const erroHandler:ErrorRequestHandler = (error,request,response,next) => {
+    if(error instanceof ValidationError){
+        let errors: ValidationErros ={
+        }
+        error.inner.forEach(err => {
+            errors[err.path]=err.errors;
+        });
+        return response.status(400).json({message:'Erro de validação de dados',errors});
+    }
+    
     console.error(error);
 
     return response.status(500).json({message:'Internal server error'});
